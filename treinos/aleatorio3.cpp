44646167a9ab88662417558a130976d0c695640b
Junio Dias <9145junio@gmail.com>
2022-09-03 15:51:57 -0300

    subindo um

---
+++ treinos/aleatorio3.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+
+
+int main(void)
+
+{
+	
+	char  lingua[100];
+	
+	printf("Escreva o nome da lingua:");
+	
+	scanf("%c", &lingua);
+	
+
+	
+	if(lingua = portugues);
+	
+	printf("Va para portugal");
+	
+	if(lingua = espanhol);
+	
+	printf("Va para a Espanha");
+	
+	if(lingua = russo);
+	
+	printf("Va para a Russia");
+	
+	else("ERRO");
+}
