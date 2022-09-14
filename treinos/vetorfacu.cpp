3f5dfab6ce12be7367c0dbd1a723d24075044523
Junio Dias <9145junio@gmail.com>
2022-09-13 23:59:03 -0300

    vetores

---
+++ treinos/vetorfacu.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	
+	
+	int j;
+	
+	int junio[5]={5,3,4,23,8};
+	
+	for(j=0;j <= 4; j++)
+	{
+		printf("junio[%d] =%d \n",j, junio[j]);
+	}
+	
+	return 0;
+}
+
+
+
---
+++ treinos/vetorfacu2.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	
+	int junio[30], j;
+	
+	for(j=0; j<=20; j++)
+	{
+		junio[j]=10;
+		
+	
+	}
+	
+	for (j=0; j <= 20; j++);
+	
+	{
+		
+		printf("vetor [%d] = %d\n", j, junio[j]);
+	}
+	
+	return 0;
+}
