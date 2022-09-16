dc1f489bca5dbf20065325409555a1e6edd58b20
Junio Dias <9145junio@gmail.com>
2022-09-16 19:27:12 -0300

    aracteres

---
+++ treinos/caracteresemC.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	char nome[10], nomes[10];
+	
+	
+	
+	
+	printf("Escreva um nome:");
+	
+	scanf("%c", &nome);
+	
+	printf("Escreva mais um nome:");
+	
+	scanf("%c", &nomes);
+	
+	printf("Os nomes escritos sao %c : \n", nome, nomes);
+}
