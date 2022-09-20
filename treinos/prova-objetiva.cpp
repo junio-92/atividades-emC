7ca20dfddcc4a87126e05668647a5c07d133f072
Junio Dias <9145junio@gmail.com>
2022-09-20 19:53:20 -0300

    prova

---
+++ treinos/prova-objetiva.cpp
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+	int a1,a2,a3,a4,a5;
+	int a6, a7, a8, a9, a10;
+	
+	int media;
+	
+printf("nota do primeiro aluno");
+
+scanf("%d", &a1); 
+
+
+printf("nota do segundo aluno aluno");
+
+scanf("%d", &a2);
+
+printf("nota do terceiro  aluno");
+
+scanf("%d", &a3);
+
+printf("nota do quarto aluno");
+
+scanf("%d", &a4);
+
+printf("nota do quinto aluno");
+
+scanf("%d", &a5);
+
+printf("nota do sexto aluno");
+
+scanf("%d", &a6);
+
+printf("nota do setimo aluno");
+
+scanf("%d", &a7);
+
+printf("nota do oitavo aluno");
+
+scanf("%d", &a8);
+
+printf("nota do nono aluno");
+
+scanf("%d", &a9);
+
+printf("nota do decimo aluno");
+
+scanf("%d", &a10);
+
+
+media = a1,a2,a3,a4,a5,a6,a7,a8,a9,a10;
+
+
+if (media >= 9);
+
+{
+	
+	
+printf("Maior nota %d", media);
+	
+	
+}
+
+
+	
+}
+	
+	
+
