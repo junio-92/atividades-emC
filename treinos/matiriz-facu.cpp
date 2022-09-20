04555a0e5f74c48fe48e008866810fccf183baa4
Junio Dias <9145junio@gmail.com>
2022-09-19 22:41:19 -0300

    matriz

---
+++ treinos/matiriz-facu.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+
+{
+	
+	int  m [2][2] = {23,56,32,45};
+	
+	printf("As linhas sao = %d \n", m [0] [1]);
+	printf("As linhas sao = %d \n", m [1][0]);
+	printf("As linhas sao = %d \n", m [1] [1]);
+	printf("As linhas sao = %d \n", m [0][0]);
+}
