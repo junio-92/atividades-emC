fb06b52fa532cc46f79e7d72340f41c7afb5ef57
Junio Dias <9145junio@gmail.com>
2022-09-22 23:47:11 -0300

    oi

--- treinos/matriz-facu2.cpp
+++ treinos/matriz-facu2.cpp
@@ -3,5 +3,20 @@
 
 int main(){
 	
-	int 
+
+	int mat[3][3], k,z;
+	
+	printf ("\n Digite um numero \n");
+	
+	for (k =0,  k<3, k++)
+	for (z =0,  z<3, z++)
+	
+	printf ("Valor [%d] [%d] = "); 
+	scanf("%d", &mat[0] [0]);
+	
+	
+	retur(0);
+	
+	
+	
 }
