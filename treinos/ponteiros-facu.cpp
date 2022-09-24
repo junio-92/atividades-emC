1e0aeadc427b930805bfefd48b4af6702e10597a
Junio Dias <9145junio@gmail.com>
2022-09-24 16:49:35 -0300

    ponteiro

---
+++ treinos/ponteiros-facu.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(void)
+
+{
+	
+	
+	int valor=30;
+	
+	int *ptb;
+	
+	ptb = &valor;
+	
+	printf (" PONTEIROS \n\n");
+	printf("Conteudo da variavel valor: %d\n", valor);
+	printf("Endereço da variavel valor: %x \n", &valor);
+	printf("Conteudo da variavel ponteiro ptb: %x", ptb);
+	
+	getch ();
+	return(0);
+}
