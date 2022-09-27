80c21aabe5718b110bcbc074c0db1a57a96cb959
Junio Dias <9145junio@gmail.com>
2022-09-26 21:59:04 -0300

    estagio

---
+++ treinos/revisao.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+//revisao
+
+
+int main()
+{
+	 int a1,a2,a3;
+	 
+	 
+	 printf("\n numero \n");
+	 
+	 scanf("%d", &a1);
+	 
+	 printf("\n Mais um numero \n");
+	 
+	 scanf("%d", &a2);
+	 
+	 a3=a1+a2;
+	 
+	 printf("\n Resultado: %d", a3);
+	 
+	 if(a3 >= 2)
+	 	
+	printf("\n\nbom");
+	 
+	 
+	else
+	printf ("pessimo");
+	 
+	 return(0);
+	
+}
